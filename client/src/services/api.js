2a07050e606bf81df5d2ebd95977bc3bee9653b0
mlsiegel <mlsiegel@ncsu.edu>
2025-06-06 16:40:35 -0500

    updating post routing

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: import.meta.env.VITE_API_URL || '/api'
 })
 
 // Attach token to every request
